2fdf2a52ee4f13ae2c764bd3bdd360d5063d61ce
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-24 10:42:25 +0300

    RecordCollection

---
+++ Js/RecordCollection.js
@@ -0,0 +1,38 @@
+'use strict';
+// Setup
+const recordCollection = {
+	2548: {
+	  albumTitle: 'Slippery When Wet',
+	  artist: 'Bon Jovi',
+	  tracks: ['Let It Rock', 'You Give Love a Bad Name']
+	},
+	2468: {
+	  albumTitle: '1999',
+	  artist: 'Prince',
+	  tracks: ['1999', 'Little Red Corvette']
+	},
+	1245: {
+	  artist: 'Robert Palmer',
+	  tracks: []
+	},
+	5439: {
+	  albumTitle: 'ABBA Gold'
+	}
+ };
+ 
+ // Only change code below this line
+ function updateRecords(records, id, prop, value) {
+ if (!value) delete records[id][prop];
+ if (prop !== "tracks" && value) records[id][prop] = value;
+ if (prop === "tracks" && value) {
+	if (!records[id].tracks) {
+		records[id].tracks = [];
+		records[id].tracks.push(value)
+	} else records[id].tracks.push(value)
+ }
+
+ 
+	return records;
+ }
+ 
+ console.log(updateRecords(recordCollection, 2468, 'tracks', 'Vlad'));
