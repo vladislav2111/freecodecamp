616955e657c0533059c9f746190a60bf6f8a88bf
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-26 12:39:01 +0300

    regex9

--- Js/rejex1.js
+++ Js/rejex1.js
@@ -26,6 +26,10 @@ let result = quoteSample.match(vowelRegex); // Change this line */
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line */
 
-let quoteSample = "Blueberry 3.141592653s are delicious.";
+/* let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line */
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aeiou]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
