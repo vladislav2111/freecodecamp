12a7b32bb1d89584e404d7d4e24083dfea812d4b
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-26 12:22:44 +0300

    rejex3

--- Js/rejex1.js
+++ Js/rejex1.js
@@ -2,6 +2,10 @@
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line */
 
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+/* let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
-let result = waldoRegex.test(waldoIsHiding);
+let result = waldoRegex.test(waldoIsHiding); */
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
