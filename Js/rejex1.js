1a5c276033a4ec8415de3088c41468b34ca5d30a
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-26 12:20:12 +0300

    rejex2

--- Js/rejex1.js
+++ Js/rejex1.js
@@ -1,3 +1,7 @@
-let myString = "Hello, World!";
+/* let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString); // Change this line
+let result = myRegex.test(myString); // Change this line */
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
