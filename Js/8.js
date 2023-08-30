d70a2408e8cdbf55de380e6b8aece646a1ebbba6
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-30 21:48:47 +0300

    new file

---
+++ Js/8.js
@@ -0,0 +1,44 @@
+// Setup
+const contacts = [
+	{
+	  firstName: "Akira",
+	  lastName: "Laine",
+	  number: "0543236543",
+	  likes: ["Pizza", "Coding", "Brownie Points"],
+	},
+	{
+	  firstName: "Harry",
+	  lastName: "Potter",
+	  number: "0994372684",
+	  likes: ["Hogwarts", "Magic", "Hagrid"],
+	},
+	{
+	  firstName: "Sherlock",
+	  lastName: "Holmes",
+	  number: "0487345643",
+	  likes: ["Intriguing Cases", "Violin"],
+	},
+	{
+	  firstName: "Kristian",
+	  lastName: "Vos",
+	  number: "unknown",
+	  likes: ["JavaScript", "Gaming", "Foxes"],
+	},
+ ];
+ 
+ function lookUpProfile(name, prop) {
+	// Only change code below this line
+	let foundName = false;
+	for (let i = 0; i < contacts.length; i++){
+		
+ if (contacts[i]['firstName'] === name ) {
+	foundName = true;
+	if (!contacts[i][prop]) return "No such property";
+	return contacts[i][prop];
+ }	
+ }
+if (!foundName) return "No such contact";
+}
+// Only change code above this line}
+ 
+ console.log(lookUpProfile("Kristian", "lastName"));
