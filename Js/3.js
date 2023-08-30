2fa9f5148c766a0be180b6c980b67b60dd482814
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-30 21:47:34 +0300

    new file

---
+++ Js/3.js
@@ -0,0 +1,95 @@
+/* let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line */
+
+/* let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding); */
+
+/* let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString); */
+
+/* let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line */
+/* 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line */
+
+/* let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line */
+
+/* let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line */
+
+/* let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line */
+
+/* let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aeiou]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line */
+
+/* let difficultSpelling = "Mississippi";
+let myRegex = /s+/gi; // Change this line
+let result = difficultSpelling.match(myRegex); */
+
+/* // Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+/* let result = chewieQuote.match(chewieRegex); */
+
+/* let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex); */
+/* let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal); */
+
+/* let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose); */
+
+/* let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/ig; // Change this line
+let result = quoteSample.match(alphabetRegexV2); */
+
+/* let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/ig; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length; */
+/* let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length; */
+/* let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length; */
+/* let username = "JackOfAllTrades";
+let userCheck = /^\W+\w+\d*$/i;// Change this line
+let result = userCheck.test(username);
+
+console.log(result); */
+
+/* let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace); */
+/* let ohStr = "Ohhh no";
+let ohRegex = /oh{3,6} no/ig; // Change this line
+let result = ohRegex.test(ohStr); */
+
+/* let timStr = "Timmmmber";
+let timRegex = /tim{4}ber/i; // Change this line
+let result = timRegex.test(timStr); */
+
+function removeDuplicateWords (s) {
+	let res = [];
+	new Set(s.split(' ')).forEach(i=> res.push(i));
+	return res.join(' ');
+ }
+
+ console.log(removeDuplicateWords('hello hi hello my my dear'));
+
