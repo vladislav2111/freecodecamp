340a1c692f580f400692be7f4fbf675a2ac33fa5
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-31 13:33:03 +0300

    essage

--- Js/20.js
+++ Js/20.js
@@ -1,5 +1,11 @@
-function titleCase(str) {
-	return str.split(' ').map(word=>`${word[0].toUpperCase()+word.slice(1).toLowerCase()}`).join(' ');
+function chunkArrayInGroups(arr, size) {
+	let newArr= [];
+	for (let i = 0; i<arr.length; i+=size){
+	  newArr.push(arr.slice(i,size+i));
+	}
+	return newArr;
  }
  
- console.log(titleCase("I'm a little tea pot"));
+
+ 
+ console.log(chunkArrayInGroups(["a", "b", "c", "d"], 3));
